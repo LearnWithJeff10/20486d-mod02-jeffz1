2530e8f35d75e11646633cfdd74386203b24196a
Jeff <jm@jeffz.com>
2020-06-05 13:20:28 -0400

    Notes from class. Please note that Photo2.cs and Comment2.cs were created using the Class Diagram. They are incomplete.

---
+++ Labfiles/DesignProject/DesignProject/Comment2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignProject
+{
+    public class Comment2
+    {
+        public int Comment2ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Photo2ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Labfiles/DesignProject/DesignProject/Photo2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignProject
+{
+    public class Photo2
+    {
+        public int Photo2ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Title
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public byte PhotoFile
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
