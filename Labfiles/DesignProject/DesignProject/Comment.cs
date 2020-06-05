2158d7a08fc8bf5098ae8647aa2f0ce873d5fd37
Jeff <jm@jeffz.com>
2020-06-04 22:09:44 -0400

    Got to Exercise 1, Task 3, Step 8 and now I'm confused.

---
+++ Labfiles/DesignProject/DesignProject/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignProject
+{
+    class Comment
+    {
+        public int CommentId { get; set; }
+        public string User { get; set; }
+        public string Subject { get; set; }
+        public int MyProperty { get; set; }
+        public int PhotoId { get; set; }
+    }
+}
